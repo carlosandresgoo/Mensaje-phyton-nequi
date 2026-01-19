3ee425be554ee9671914aee331e421c20bc7b4b1
Carlos Andrés Ruiz Hinestroza <123417384+carlosandresgoo@users.noreply.github.com>
2026-01-19 07:25:20 -0500

    test
    test

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -21,12 +21,13 @@ def test_create_message():
 def test_get_messages():
     session_id = "sess-456"
     # Añadimos headers=HEADERS a la petición GET
-    response = client.get(f"/api/messages/{session_id}", headers=HEADERS)
+    response = client.get(f"/api/messages?session_id={session_id}", headers=HEADERS)
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
 def test_unauthorized_access():
     # Prueba que sin la API Key el sistema rechaza la conexión
-    response = client.get("/api/messages/sess-456") # Sin headers
+    session_id = "sess-456"
+    response = client.get(f"/api/messages?session_id={session_id}") # Sin headers
     assert response.status_code == 401
     assert response.json()["detail"] == "Credenciales de API inválidas"
